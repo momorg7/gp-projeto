fe773487315b1a19b21c71233a12fbcde6fc99d0
Morgana <30328071+momorg7@users.noreply.github.com>
2019-10-31 09:53:02 -0300

    Conexao ao mongodb feita.

--- project/index.js
+++ project/index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 require('dotenv/config')
+const mongoose = require('mongoose');
+
 const app = express()
+
+mongoose.connect('mongodb://localhost/postsDatabase');
+let db = mongoose.connection;
+
+db.once('open', ()=>{
+    console.log('Conectado ao Mongodb');
+});
+
+db.on('error', (err)=>{
+    console.log(err);
+});
 
 app.get('/', (req, res) => {
     res.json('oiii')
