b158cab5a69c26c9be3377a8a1a7a8499327c2ef
Morgana <30328071+momorg7@users.noreply.github.com>
2019-11-08 10:37:10 -0300

    View Users finalizada.

--- project/controller/UsersController.js
+++ project/controller/UsersController.js
@@ -40,15 +40,40 @@ module.exports = {
         });
     },
 
-    async update(req, res) {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    
-        return res.json(user);
+    async updateGet(req, res){
+        User.findById(req.params.id, (err, user)=>{
+            if(err){
+                console.log(err);
+                return;
+            }
+            else{
+                res.render('update_user', {
+                    user
+                });
+            }
+        });
+    },
+
+    async updatePost(req, res) {
+        User.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.redirect('/');
+            }
+        });
     },
 
     async destroy(req, res) {
-        await User.findByIdAndRemove(req.params.id);
-
-        return res.send();
+        User.findByIdAndRemove(req.params.id, (err)=>{
+            if(err){
+                console.log(err);
+                return;
+            }
+            else{
+                res.redirect('/');
+            }
+        });
     }
 }
--- project/routes/usersRouter.js
+++ project/routes/usersRouter.js
@@ -4,12 +4,16 @@ let db = require('../mongoConnection');
 let UsersController = require('../controller/UsersController');
 const routes = express.Router();
 
-routes.get('/', UsersController.index);
-
-routes.get('/create', UsersController.storeGet);
+// CREATE
 routes.post('/create', UsersController.storePut);
-
+routes.get('/create', UsersController.storeGet);
+// READ
 routes.get('/', UsersController.index);
 routes.get('/:id', UsersController.show);
+// UPDATE
+routes.get('/edit/:id', UsersController.updateGet);
+routes.post('/edit/:id', UsersController.updatePost);
+// DELETE
+routes.get('/delete/:id', UsersController.destroy);
 
 module.exports = routes;
