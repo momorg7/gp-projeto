29a445151d0fd3b1c5e375732cec226394267bf8
Morgana <30328071+momorg7@users.noreply.github.com>
2019-11-25 16:54:57 -0300

    PEGOU

--- project/routes/postsRouter.js
+++ project/routes/postsRouter.js
@@ -3,10 +3,29 @@ const routes = express.Router();
 //const mongoose = require('mongoose');
 let db = require('../mongoConnection');
 let PostsController = require('../controller/PostsController');
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+
+        // error first callback
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+
+        // error first callback
+        cb(null, `${file.fieldname}-${Date.now()}.${path.extname(file.originalname)}`);
+        //cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+// utiliza a storage para configurar a instância do multer
+const upload = multer({ storage });
 
 // CREATE
 routes.get('/create', PostsController.ensureAuthenticated, PostsController.createGet);
-routes.post('/create', PostsController.createPost);
+routes.post('/create', upload.single('file'), PostsController.createPost);
 
 // READ ONLY
 routes.get('/:id', PostsController.ensureAuthenticated, PostsController.readGet);
